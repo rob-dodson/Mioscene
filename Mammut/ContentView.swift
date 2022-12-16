115f30f203c1596192c2d38a4bd1ac9dea8e4dac
Robert Dodson <rad@robdodson.net>
2022-12-16 13:12:29 -0800

    Initial Commit

---
+++ Mammut/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mammut
+//
+//  Created by Robert Dodson on 12/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mammut/MammutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MammutApp.swift
+//  Mammut
+//
+//  Created by Robert Dodson on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MammutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
