214913e38dabc29247288c25a0f6751b2bfd0889
Robert Dodson <rad@robdodson.net>
2022-12-21 11:25:56 -0800

    Finally timeline switching working

--- Mammut/ContentView.swift
+++ Mammut/ContentView.swift
@@ -13,7 +13,10 @@ struct ContentView: View
     @ObservedObject var mast : Mastodon
     
     @State private var selectedTimeline : TimeLine = .home
-    @State private var stats = [MStatus]()
+    @State private var stats1 = [MStatus]()
+    @State private var stats2 = [MStatus]()
+    @State private var stats3 = [MStatus]()
+    @State private var stats4 = [MStatus]()
     
     var body: some View
     {
@@ -22,11 +25,21 @@ struct ContentView: View
         {
             ScrollView
             {
-                ForEach(stats)
+                ForEach(getstats(timeline: $selectedTimeline))
                 { mstat in
                     Post(mstat:mstat)
                         .padding(.horizontal)
                         .padding(.top)
+                }
+            }
+            .task
+            {
+                fetchStatuses(timeline: TimeLine.home)
+                Task
+                {
+                    fetchStatuses(timeline: TimeLine.localTimeline)
+                    fetchStatuses(timeline: TimeLine.publicTimeline)
+                    fetchStatuses(timeline: TimeLine.tag)
                 }
             }
         }
@@ -55,7 +68,7 @@ struct ContentView: View
                             {
                                 ForEach(TimeLine.allCases)
                                 { timeline in
-                                       Text(timeline.rawValue.capitalized)
+                                    Text(timeline.rawValue.capitalized)
                                 }
                             }
                 }
@@ -109,14 +122,37 @@ struct ContentView: View
         }
     }
     
-
-    func fetchStatuses(timeline:Binding<TimeLine>) -> [MStatus]
+    func getstats(timeline:Binding<TimeLine>) -> [MStatus]
     {
-        mast.getTimeline(timeline: selectedTimeline,done:
+        switch timeline.wrappedValue
+        {
+        case .home:
+            return stats1
+        case .localTimeline:
+            return stats2
+        case .publicTimeline:
+            return stats3
+        case .tag:
+            return stats4
+        }
+    }
+        
+    func fetchStatuses(timeline:TimeLine)
+    {
+        mast.getTimeline(timeline: timeline,done:
         { newstats in
-            stats = newstats
+            switch timeline
+            {
+            case .home:
+                stats1 = newstats
+            case .localTimeline:
+                stats2 = newstats
+            case .publicTimeline:
+                stats3 = newstats
+            case .tag:
+                stats4 = newstats
+            }
         })
     }
-
 }
 
