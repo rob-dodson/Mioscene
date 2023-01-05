b3a1ca5090277bb2c4e724f135093f450623df65
Robert Dodson <rad@robdodson.net>
2023-01-05 13:02:23 -0800

    Create custom tabbar

--- Mammut/ContentView.swift
+++ Mammut/ContentView.swift
@@ -15,42 +15,131 @@ struct ContentView: View
     
     @EnvironmentObject var settings: Settings
     
-    @State private var tabselection = 0
+    @State var tabIndex = 0
     
     var body: some View
     {
-        TabView(selection: $tabselection)
+        VStack
         {
-            TimeLineView(mast: mast)
-            .tabItem
-            {
-                Label("Timelines", systemImage: "house.fill")
-            }
-            .tag(0)
+            CustomTopTabBar(tabIndex: $tabIndex)
             
-            AccountView(mast: mast)
-            .tabItem
+            switch tabIndex
             {
-                Label("Accounts", systemImage: "person.crop.circle")
+            case 0: TimeLineView(mast: mast)
+            case 1: AccountView(mast: mast)
+            case 2: SearchView(mast: mast)
+            case 3: SettingsView(mast: mast)
+            default:
+                TimeLineView(mast: mast)
             }
-            .tag(1)
-            
-            SearchView(mast: mast)
-            .tabItem
-            {
-                Label("Search", systemImage: "magnifyingglass")
-            }
-            .tag(2)
-            
-            SettingsView(mast: mast)
-            .tabItem
-            {
-                Label("Settings", systemImage: "gear")
-            }
-            .tag(3)
+            Spacer()
         }
-        .accentColor(settings.theme.accentColor)
-        .padding()
+        .frame(width: 500, alignment: .center)
+        .padding(.horizontal, 12)
     }
 }
 
+       
+
+struct CustomTopTabBar: View
+{
+    @Binding var tabIndex: Int
+    var body: some View
+    {
+        Spacer()
+        HStack(spacing: 20)
+        {
+            Spacer()
+            
+            TabBarButton(text: "Timelines", isSelected: .constant(tabIndex == 0))
+                .onTapGesture { onButtonTapped(index: 0) }
+            
+            TabBarButton(text: "Accounts", isSelected: .constant(tabIndex == 1))
+                .onTapGesture { onButtonTapped(index: 1) }
+            
+            TabBarButton(text: "Search", isSelected: .constant(tabIndex == 2))
+                .onTapGesture { onButtonTapped(index: 2) }
+            
+            TabBarButton(text: "Settings", isSelected: .constant(tabIndex == 3))
+                .onTapGesture { onButtonTapped(index: 3) }
+                         
+            Spacer()
+        }
+        .border(width: 1, edges: [.bottom], color: .black)
+    }
+    
+    private func onButtonTapped(index: Int)
+    {
+        withAnimation { tabIndex = index }
+    }
+}
+
+struct TabBarButton: View
+{
+    @EnvironmentObject var settings: Settings
+    
+    let text: String
+    
+    @Binding var isSelected: Bool
+    
+    var body: some View
+    {
+        Text(text)
+            .fontWeight(isSelected ? .heavy : .regular)
+            .font(.title2)
+            .foregroundColor(isSelected ? settings.theme.accentColor : settings.theme.minorColor)
+            .padding(.bottom,10)
+            .border(width: isSelected ? 4 : 1, edges: [.bottom], color: .black)
+    }
+}
+
+
+
+
+
+    struct EdgeBorder: Shape {
+    
+        var width: CGFloat
+        var edges: [Edge]
+    
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            for edge in edges {
+                var x: CGFloat {
+                    switch edge {
+                    case .top, .bottom, .leading: return rect.minX
+                    case .trailing: return rect.maxX - width
+                    }
+                }
+    
+                var y: CGFloat {
+                    switch edge {
+                    case .top, .leading, .trailing: return rect.minY
+                    case .bottom: return rect.maxY - width
+                    }
+                }
+    
+                var w: CGFloat {
+                    switch edge {
+                    case .top, .bottom: return rect.width
+                    case .leading, .trailing: return self.width
+                    }
+                }
+    
+                var h: CGFloat {
+                    switch edge {
+                    case .top, .bottom: return self.width
+                    case .leading, .trailing: return rect.height
+                    }
+                }
+                path.addPath(Path(CGRect(x: x, y: y, width: w, height: h)))
+            }
+            return path
+        }
+    }
+    
+    extension View {
+        func border(width: CGFloat, edges: [Edge], color: SwiftUI.Color) -> some View {
+            overlay(EdgeBorder(width: width, edges: edges).foregroundColor(color))
+        }
+    }
