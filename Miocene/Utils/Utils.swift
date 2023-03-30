acc5cb091335e04039ebce1c84e7c87da4462d01
Robert Dodson <rad@robdodson.net>
2023-03-30 13:07:51 -0700

    Move Log from Utils.swift to it's own file.

--- Miocene/Utils/Utils.swift
+++ Miocene/Utils/Utils.swift
@@ -9,69 +9,6 @@ import Foundation
 import SwiftUI
 
 
-class Log
-{
-    static var fileName : String?
-    
-    static func logAlert(errorType:MioceneError,msg:String)
-    {
-        Log.log(msg:msg)
-        AlertSystem.shared?.reportError(type:errorType, msg: msg)
-    }
-
-
-    static func log(msg:String)
-    {
-       log(msg: msg, rewindfile: false)
-    }
-    
-    
-    static func log(msg:String, rewindfile:Bool)
-    {
-        print("debug: \(msg)")
-        
-        do
-        {
-            if fileName == nil
-            {
-                let logdir = try FileManager.default
-                    .url(for: .applicationSupportDirectory,
-                         in: .userDomainMask,
-                         appropriateFor: nil,
-                         create: true)
-                    .appendingPathComponent("Miocene")
-                
-                fileName = "\(logdir.path)/Logging.txt"
-            }
-            
-            if let filepath = fileName
-            {
-                let logmsg = "\(Date().description): \(msg)"
-                
-                if rewindfile == true
-                {
-                    try FileManager.default.removeItem(atPath: filepath)
-                }
-                
-                if FileManager.default.fileExists(atPath: filepath) == false
-                {
-                    FileManager.default.createFile(atPath: filepath, contents: nil, attributes: nil)
-                }
-                
-                guard let file = FileHandle.init(forWritingAtPath: filepath) else { return }
-                
-                file.seekToEndOfFile()
-                file.write(logmsg.data(using: .utf8)!)
-                file.write("\n".data(using: .utf8)!)
-                file.closeFile()
-            }
-        }
-        catch
-        {
-            print("write to debug file failed. error: \(error) - msg: \(msg)")
-        }
-    }
-}
 
 
 func SpacerLine(color:Color) -> some View
